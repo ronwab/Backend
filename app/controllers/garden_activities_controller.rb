04db04c0a2d40617ea5da3e253f675f39af5e7f8
Ronald Wabukenda <ronald.wabukenda@salesloft.com>
2019-11-12 14:27:11 -0500

    Added garden activity logic

--- app/controllers/garden_activities_controller.rb
+++ app/controllers/garden_activities_controller.rb
@@ -1,3 +1,37 @@
-class GardenActivities < ApplicationController
+class GardenActivitiesController < ApplicationController
+  respond_to :json
+
+  def index
+    respond_with GardenActivity.all
+  end
+
+  def show
+    respond_with my_garden_activity
+  end
+
+  def create
+    new_garden_activity = GardenActivity.create!(garden_activities_params)
+
+    respond_with new_garden_activity
+  end
+
+  def destroy
+    my_garden_activity.destroy
+    binding.pry
+    # HOW WILL THIS WORK
+    # respond_with json: { message: `#{@garden_activity.id} is destroyed`}
+    respond_with json: { message: " Record is destroyed" }
+  end
+
+  private
+
+  def my_garden_activity
+    @garden_activity ||= GardenActivity.find(params[:id])
+  end
+
+  def garden_activities_params
+    params.permit(:activity_id, :garden_id, :date_performed, :weather, :notes)
+  end
+
 
 end
--- spec/factories/garden_activities.rb
+++ spec/factories/garden_activities.rb
@@ -9,3 +9,13 @@ FactoryGirl.define do
   end
 
 end
+
+#
+#
+# FactoryGirl.define do
+#   factory :fertilizer do
+#     fertilizer_name { Faker::Science.element }
+#     date_bought { Faker::Date.backward(days: 24) }
+#     quantity { Faker::Number.decimal(l_digits: 2) }
+#   end
+# end

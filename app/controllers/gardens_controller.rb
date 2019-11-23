5819d086040080e40c223409a9b63644feb5375d
Ronald Wabukenda <ronald.wabukenda@salesloft.com>
2019-11-22 20:43:52 -0500

    added garden specs

--- app/controllers/gardens_controller.rb
+++ app/controllers/gardens_controller.rb
@@ -1,63 +1,54 @@
-# frozen_string_literal: true
-
 class GardensController < ApplicationController
   respond_to :json
 
-  # ensure user is logged in before they can edit, delete create, or update.
-  # before_action :authenticate_user!, except: %i[show index]
-  # GardenFertilizer.where(garden_id: 16)
-
-  def fertilized_gardens
-    # why is this ID and not fertilizer_id??
-    respond_with @garden = Fertilizer.find(params[:id]).gardens
-  rescue ActiveRecord::RecordNotFound => e
-    render json: { error: e.message }, status: :unprocessable_entity
-  end
-
-  def applied_fertilizers
-    respond_with @garden = Garden.find(params[:id]).fertilizers
-  rescue ActiveRecord::RecordNotFound => e
-    render json: { error: e.message }, status: :unprocessable_entity
-  end
 
   def index
-    respond_with @garden = Garden.all.page(1)
-    #
-    # ****** Why does this fail ??  Fertilizer.find(fertilizer_id).gardens || Garden.all
-    # gardens = Garden.all || Fertilizer.find(fertilizer_id).gardens
-    # respond_with gardens
+    @garden = Garden.all #.page(1)
+    respond_with(@garden)
   end
 
   def create
-    # binding.pry
     new_garden = Garden.create!(garden_params)
     respond_with(new_garden)
   rescue ActiveRecord::RecordInvalid => e
-    render json: { error: e.message }, status: :unprocessable_entity
+    render json: {error: e.message}, status: :unprocessable_entity
   end
 
   def show
     respond_with garden
   rescue ActiveRecord::RecordNotFound => e
     Rails.logger.info "Error rescued in show method  #{e.message}"
-    render json: { error: e.message }, status: :unprocessable_entity
+    render json: {error: e.message}, status: :unprocessable_entity
   end
 
   def destroy
     garden.destroy
-    render json: { "message": "The  #{garden_params[:plant_name]}  garden has been destroyed" }
+    render json: {"message": "The  #{garden_params[:plant_name]}  garden has been destroyed"}, status: :ok
     Rails.logger.info "Deleted record with #{garden}"
   rescue ActiveRecord::RecordNotFound => e
     Rails.logger.info "Error rescued in Delete method #{e.message}"
-    render json: { error: e.message }, status: :unprocessable_entity
+    render json: {error: e.message}, status: :unprocessable_entity
   end
 
   def update
     garden.update(garden_params)
-    render json: { "message": "The #{garden_params[:plant_name]} garden has been updated" }
+    render json: {"message": "The #{garden_params[:plant_name]} garden has been updated"}
   rescue ActiveRecord::RecordNotFound => e
     Rails.logger.info "Error rescued in update method #{e.message}"
-    render json: { error: e.message }, status: :unprocessable_entity
+    render json: {error: e.message}, status: :unprocessable_entity
+  end
+
+  def fertilized_gardens
+    # why is this ID and not fertilizer_id??
+    respond_with @garden = Fertilizer.find(params[:id]).gardens
+  rescue ActiveRecord::RecordNotFound => e
+    render json: {error: e.message}, status: :unprocessable_entity
+  end
+
+  def applied_fertilizers
+    respond_with @garden = Garden.find(params[:id]).fertilizers
+  rescue ActiveRecord::RecordNotFound => e
+    render json: {error: e.message}, status: :unprocessable_entity
   end
 
   def garden
---
+++ spec/controllers/gardens_controller_spec.rb
@@ -0,0 +1,59 @@
+require 'rails_helper';
+RSpec.describe GardensController, type: :controller do
+
+
+  describe "get index" do
+    it ' should return the correct number of objects ' do
+      @mygarden = FactoryGirl.create(:garden)
+      get :index, format: :json
+      response_body = JSON.parse(response.body)
+      expect(response.status).to eq(200)
+      expect(response_body.size).to eq(1)
+      expect(response_body.first['plant_name']).to eq(@mygarden.plant_name)
+    end
+    it 'should error out if there are no gardens' do
+      get :index, format: :json
+      expect(response.status).to eq(200)
+      expect(response.body).to eq("[]")
+    end
+  end
+  describe 'Create a new garden' do
+    it 'should create a new garden' do
+
+      expect { post :create, format: :json,
+                    params: {plant_name: "Cucumber", date_planted: Time.now, rows: 2,
+                             spacing: 4, notes: "Kill my boss? Do I dare live out"} }.to change { Garden.count }.by(1)
+      expect(response.status).to eq(201)
+
+    end
+  end
+  describe 'Delete the garden' do
+    let!(:garden) { FactoryGirl.create(:garden) }
+    it 'should delete a single garden' do
+      expect { delete :destroy, params: {id: garden.id} }.to change(Garden, :count).by(-1)
+      expect(response.status).to eq(200)
+    end
+    it 'should throw error if record does not exist' do
+      delete :destroy, params: {id: 9999999}
+      expect(response.body).to include("Couldn't find Garden with 'id'=#{9999999}")
+      expect(response.status).to eq(422)
+    end
+  end
+  describe 'search for garden' do
+    let!(:garden) { FactoryGirl.create(:garden) }
+    it 'should display returned garden ' do
+
+    get :show, format: :json,  params: {id: garden.id}
+    expect(response.status).to eq(200)
+    binding.pry
+      expect (JSON.parse(response.body)).to eq(garden)
+
+    end
+    it 'should throw an error if no garden is returned' do
+      patch :update, params: {id: 34234234}
+      expect(response.status).to eq(422)
+      expect(response.body).to include("Couldn't find Garden with 'id'=#{34234234}")
+    end
+
+  end
+end
